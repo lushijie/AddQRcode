4921b4dd4a508fe2f0bd6f2b54dee253f66a1d94
lushijie <lushijie03@meituan.com>
2018-08-09 15:48:43 +0800

    crx

--- static/js/content_script.js
+++ static/js/content_script.js
@@ -55,16 +55,6 @@
 // 	}
 // }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    // if (request.greeting == "hello")//判断是否为要处理的消息
-      // sendResponse({farewell: "goodbye"});
-});
-
-
 
 // //console.log(chrome.runtime.id);
 
