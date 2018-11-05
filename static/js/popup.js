d8d1eaafdde7f3c3c149233a64a48393c6764d8b
lushijie <lushijie03@meituan.com>
2018-11-05 10:45:39 +0800

    自定义位置

--- static/js/popup.js
+++ static/js/popup.js
@@ -4,13 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
   var $base64img = '';
 
   // 从缓存中读取内容
-  var __DB__ = JSON.parse(bg.localStorage.getItem('__DB__') || '{}');
+  var __DB__ = JSON.parse(bg.localStorage.getItem('__QrCodeDb__') || '{}');
   if (__DB__.action === 'store') {
     $('#qrfileinput').val(__DB__.$qrfileinput);
     $('#qrwidth').val(__DB__.$qrwidth) || 200;
     $('#qrmargin').val(__DB__.$qrmargin) || 40;
     $('#qrpos').val(__DB__.$qrpos) || 4;
     $('#qrlink').val(__DB__.$qrlink);
+    $('#qrleft').val(__DB__.$qrleft);
+    $('#qrtop').val(__DB__.$qrtop);
+    $('.custom-pos-group')[+$('#qrpos').val() === 6 ? 'show' : 'hide']();
+    $('.qr-margin-group')[+$('#qrpos').val() === 6 ? 'hide' : 'show']();
 
     // 恢复图片
     if (__DB__.$base64img) {
@@ -34,37 +38,45 @@ document.addEventListener('DOMContentLoaded', function () {
       ctx.drawImage(img, 0, 0, img.width, img.height);
       $('#preview').attr('src', canvas.toDataURL('image/png'));
 
-      // 绘制二维码
-      var storeData = getStoreData();
-      if (!storeData.$qrlink) {
-        return;
-      }
-
       var pos = {
         x: 0,
         y: 0
       };
+
+      // 绘制二维码
+      var storeData = getStoreData();
+      // $qrlink 二维码链接
+      if (!storeData.$qrlink) {
+        return;
+      }
+
+      // $qrwidth 二维码尺寸
+      // $qrmagin 二维码边距
       const distance = storeData.$qrwidth + storeData.$qrmargin;
       switch (storeData.$qrpos) {
-        case 1:
+        case 1: // 左上角
           pos.x = storeData.$qrmargin;
           pos.y = storeData.$qrmargin;
         break;
-        case 2:
+        case 2: // 右上角
           pos.x = canvas.width - distance;
           pos.y = storeData.$qrmargin;
         break;
-        case 3:
+        case 3: // 左下角
           pos.x = storeData.$qrmargin;
           pos.y = canvas.height - distance;
         break;
-        case 4:
+        case 4: // 右下角
           pos.x = canvas.width - distance;
           pos.y = canvas.height - distance;
         break;
-        case 5:
+        case 5: // 居中
           pos.x = canvas.width/2 - +storeData.$qrwidth/2;
           pos.y = canvas.height/2 - +storeData.$qrwidth/2;
+        break;
+        case 6: // 自定义
+          pos.x = storeData.$qrleft;
+          pos.y = storeData.$qrtop;
         break;
       }
 
@@ -107,9 +119,10 @@ document.addEventListener('DOMContentLoaded', function () {
       $qrwidth : +$('#qrwidth').val() || 200,
       $qrmargin : +$('#qrmargin').val() || 40,
       $qrpos : +$('#qrpos').val() || 4,
-      $qrlink : $('#qrlink').val() || ''
+      $qrlink : $('#qrlink').val() || '',
+      $qrleft: $('#qrleft').val() || 0,
+      $qrtop: $('#qrtop').val() || 0
     };
-
   }
 
   // 更新缓存
@@ -134,6 +147,8 @@ document.addEventListener('DOMContentLoaded', function () {
     $('#qrpos').val(4);
     $('#qrlink').val('');
     $('#preview').attr('src', '');
+    $('#qrtop').val(0);
+    $('#qrleft').val(0);
     $base64img = '';
 
     // 更新缓存
@@ -164,12 +179,14 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // watch
-  $('#qrfile, #qrpos, #qrwidth, #qrmargin').change(function(evt) {
+  $('#qrfile, #qrpos, #qrwidth, #qrmargin, #qrleft, #qrtop').change(function(evt) {
+    $('.custom-pos-group')[+$('#qrpos').val() === 6 ? 'show' : 'hide']();
+    $('.qr-margin-group')[+$('#qrpos').val() === 6 ? 'hide' : 'show']();
     render();
   });
 
   // watch
-  $('#qrlink, #qrwidth, #qrmargin').keyup(function(evt) {
+  $('#qrlink, #qrwidth, #qrmargin, #qrleft, #qrtop').keyup(function(evt) {
     render();
   });
 });
